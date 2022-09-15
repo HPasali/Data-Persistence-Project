5af4dd49ddd39c58ce5dad05235e15b8a3f0aa66
Hasan Paşalı <hasan-pasali@hotmail.com>
2022-09-15 19:58:46 +0300

    Update

--- Assets/Scripts/MainManager.cs
+++ Assets/Scripts/MainManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using System.IO;
 
 public class MainManager : MonoBehaviour
 {
@@ -12,20 +13,35 @@ public class MainManager : MonoBehaviour
 
     public Text ScoreText;
     public GameObject GameOverText;
-    
-    private bool m_Started = false;
-    private int m_Points;
-    
-    private bool m_GameOver = false;
 
-    
+    private bool m_Started = false;
+    private bool nameEntered = false;
+    private int m_Points;
+    private int bestPoint = 0;
+    private bool m_GameOver = false;
+    public static MainManager Instance;
+    public string inputName;
+
+    private void Awake()
+    {
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+
+
     // Start is called before the first frame update
     void Start()
     {
         const float step = 0.6f;
         int perLine = Mathf.FloorToInt(4.0f / step);
-        
-        int[] pointCountArray = new [] {1,1,2,2,5,5};
+
+        int[] pointCountArray = new[] { 1, 1, 2, 2, 5, 5 };
         for (int i = 0; i < LineCount; ++i)
         {
             for (int x = 0; x < perLine; ++x)
@@ -40,7 +56,7 @@ public class MainManager : MonoBehaviour
 
     private void Update()
     {
-        if (!m_Started)
+        if (!m_Started && nameEntered)
         {
             if (Input.GetKeyDown(KeyCode.Space))
             {
@@ -71,6 +87,42 @@ public class MainManager : MonoBehaviour
     public void GameOver()
     {
         m_GameOver = true;
+        SaveScore();
         GameOverText.SetActive(true);
     }
+
+    [System.Serializable]
+    class SaveData
+    {
+        public string input;
+        public int highestScore;
+
+    }
+
+    public void SaveName()
+    {
+        SaveData saveData = new SaveData();
+        saveData.input = inputName;
+
+        string json = JsonUtility.ToJson(saveData);
+        File.WriteAllText(Application.persistentDataPath + "/savefile.json", json);
+    }
+
+    public void SaveScore()
+    {
+        SaveData saveData = new SaveData();
+        if(m_Points > saveData.highestScore)
+        {
+            saveData.highestScore = m_Points;
+        }
+        else
+        {
+            return;
+        }
+
+        string json = JsonUtility.ToJson(saveData);
+        File.WriteAllText(Application.persistentDataPath + "/savefile.json", json);
+    }
+
+
 }
